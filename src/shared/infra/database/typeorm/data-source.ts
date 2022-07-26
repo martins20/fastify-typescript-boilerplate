1162db003395614265b871094355b56b295d4a2e
martins20 <martins20.tech@gmail.com>
2022-07-26 19:08:58 -0300

    setup: synchronyse false

--- src/shared/infra/database/typeorm/data-source.ts
+++ src/shared/infra/database/typeorm/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   migrationsRun: true,
   entities: [
